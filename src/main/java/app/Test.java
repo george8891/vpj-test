54072a27ae1dfec86704ef3a3a90e4c7f6ef705a
Georgi P <georgi.penchev1999@gmail.com>
2026-09-02 15:35:26 +0300

    for cycle

--- src/main/java/app/Test.java
+++ src/main/java/app/Test.java
@@ -12,5 +12,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
